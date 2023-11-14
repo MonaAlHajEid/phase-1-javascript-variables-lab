66eee45d81e20c3472b5da8d4271f996c6fc4b92
Mona <monabadee@gmail.com>
2023-11-14 22:00:25 +0300

    Complete Variables Lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+// Declare and assign values to variables
+// Declare and assign values to variables
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
